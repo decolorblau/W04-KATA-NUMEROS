a2a84b21b064fed9981464592b417c089dd4f6c8
Claudia Font <claudiafontmarques@gmail.com>
2021-10-18 21:04:00 +0200

    Added funcionality of buttons

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Button from "./components.js/Button";
 import Number from "./components.js/Number";
 import { useState } from "react";
 
 function App() {
-  const boxNum = [
+  /*   const boxNum = [
     { id: 1, number: 1 },
     { id: 2, number: 2 },
     { id: 3, number: 3 },
     { id: 4, number: 4 },
     { id: 5, number: 5 },
-  ];
-  const [number, setNumber] = useState(1);
+  ]; */
+
+  const [boxNum, setBoxNum] = useState([
+    { id: 1, number: 1 },
+    { id: 2, number: 2 },
+    { id: 3, number: 3 },
+    { id: 4, number: 4 },
+    { id: 5, number: 5 },
+  ]);
 
   const incrementar = () => {
-    if (number < 20) {
-      setNumber(number + 1);
+    if (boxNum[4].number < 20) {
+      const nextBoxNum = boxNum.map((element) => {
+        const container = {};
+        container.number = element.number += 1;
+        container.id = element.id += 1;
+
+        return container;
+      });
+      setBoxNum(nextBoxNum);
     }
   };
+
   const decrementar = () => {
-    if (number > 0) {
-      setNumber(number - 1);
+    if (boxNum[0].number > 0) {
+      const nextBoxNum = boxNum.map((element) => {
+        const container = {};
+        container.number = element.number -= 1;
+        container.id = element.id -= 1;
+
+        return container;
+      });
+      setBoxNum(nextBoxNum);
     }
   };
 
